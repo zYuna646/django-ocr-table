feb2401ba23505a4ddb11256ccdc1bd8103ad7d5
zYuna646 <anggarangga646@gmail.com>
2024-08-04 15:00:46 +0800

    fix api

--- ocrapp/utils.py
+++ ocrapp/utils.py
@@ -1,21 +1,13 @@
 import requests
 import numpy as np
 from paddleocr import PaddleOCR
-from openpyxl import Workbook
 from PIL import Image
 import os
 from io import BytesIO
-from django.http import FileResponse
-from django.http import HttpResponseServerError
-import traceback
+import json
 
-
-def load_image(image):
-    if hasattr(image, 'url') and image.url.startswith('http'):
-        response = requests.get(image.url)
-        img = Image.open(BytesIO(response.content))
-    else:
-        img = Image.open(image)
+def load_image(image_path):
+    img = Image.open(image_path)
     return img
 
 def extract_text_from_image(image):
@@ -45,25 +37,49 @@ def group_text_by_rows(results):
         row.sort(key=lambda x: x[0])
         table_data.append([text for _, text in row])
     
-    return table_data
+    # Map table data to the desired JSON structure
+    json_data = {}
+    for i, row in enumerate(table_data):
+        if len(row) >= 18:  # Ensure there are enough columns
+            data = {
+                "Akta": {
+                    "No.": row[0],
+                    "Tanggal": row[1],
+                },
+                "Bentuk_Perbuatan_Hukum": row[2],
+                "NPWP": {
+                    "Pihak_Yang_Mengalihkan/Memberikan": row[3],
+                    "Pihak Yang Menerima": row[4],
+                },
+                "Jenis_Dan_Nomor_Hak": row[5],
+                "Letak_Tanah_Dan_Bangunan": row[6],
+                "Luas": {
+                    "Tanah": row[7],
+                    "Bgn": row[8],
+                },
+                "Harga_Transaksi_Perolehan_Pengalihan": row[9],
+                "SPPT_PPB": {
+                    "NOP_Tahun": row[10],
+                    "NJOP": row[11],
+                },
+                "SSP": {
+                    "Tgl": row[12],
+                    "Rp": row[13],
+                },
+                "SSB": {
+                    "Tgl": row[14],
+                    "Rp": row[15],
+                },
+                "Ket": row[16]
+            }
+            json_data[i] = data
+    
+    return json_data
 
-def results_to_excel(table_data, output_excel):
-    wb = Workbook()
-    ws = wb.active
-
-    for row in table_data:
-        ws.append(row)
-
-    wb.save(output_excel)
-    return output_excel
-
-
-def process_image(image_path, output_excel):
+def process_image(image_path):
     os.environ['KMP_DUPLICATE_LIB_OK']='True'
     image = load_image(image_path)
     results = extract_text_from_image(image)
-    table_data = group_text_by_rows(results)
-    excel_file = results_to_excel(table_data, output_excel)
-    return excel_file  # Mengembalikan nama file string
-
-
+    
+    json_data = group_text_by_rows(results)
+    return json_data

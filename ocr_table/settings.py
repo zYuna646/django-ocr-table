618877f4c5b993ab2d723230bdc88af68e72cb7d
zYuna646 <anggarangga646@gmail.com>
2024-08-22 11:28:12 +0800

    fix

--- ocr_table/settings.py
+++ ocr_table/settings.py
@@ -30,7 +30,8 @@ ALLOWED_HOSTS = [
     '.ngrok-free.app',  # For ngrok-free.app domains
     'localhost',
     '127.0.0.1',
-    '81.16.28.234'
+    '81.16.28.234',
+    'ocr.api.noctdev.tech'
     # Add any other domains you want to allow
 ]
 
